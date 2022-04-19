a82c4fed6bd176af313bfaf75e41de1ba9a0dc1b
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-04-19 13:02:39 +1000

    Basic header (with NavBar) and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Container} from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
     <>
-    <h1> Welcome to Shopping App</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+
+        <h1> Welcome to Shopping App</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
