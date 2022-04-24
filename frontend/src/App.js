75685338a2619f6dc36655e7198f92d0c2b87456
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-04-24 22:35:54 +1000

    Added FrontEnd Code for user login
    Added constants, reducers and actions for the user to log in and log out, Changed Header code to include dropdown for profile view and logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
@@ -15,10 +16,11 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Routes>
-              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/login' element={<LoginScreen />} />
               <Route path='/product/:id' element={<ProductScreen />} />
               <Route path='/cart/:id' element={<CartScreen />} />
               <Route path='/cart' element={<CartScreen />} />
+              <Route path='/' element={<HomeScreen />} exact />
             </Routes>
           </Container>
         </main>
