5bc1d642e4fcf5eab81d53e8779531ff6bf002a2
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-04-26 21:27:10 +1000

    Added frontend to see order summary before payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   const userLogin = useSelector((state) => state.userLogin);
@@ -24,6 +25,7 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Routes>
+              <Route path='/order/:id' element={<OrderScreen />} />
               <Route
                 path='/shipping'
                 element={userInfo ? <ShippingScreen /> : <LoginScreen />}
