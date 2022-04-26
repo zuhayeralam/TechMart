6846af1020935d518c93c67da898b4640dbcf9cc
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-04-26 22:39:41 +1000

    Added backend code to get order details
    user will be able to see their orders beside their profile. added backend code to get all user related orders

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PayPalButton } from 'react-paypal-button-v2';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
@@ -9,8 +9,9 @@ import Loader from '../components/Loader';
 import { getOrderDetails, payOrder } from '../actions/orderActions';
 import { ORDER_PAY_RESET } from '../constants/orderConstants';
 
-const OrderScreen = ({ match }) => {
-  const orderId = match.params.id;
+const OrderScreen = () => {
+  const params = useParams();
+  const orderId = params.id;
 
   const [sdkReady, setSdkReady] = useState(false);
 
