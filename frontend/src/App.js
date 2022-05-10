c7c2013410a2a1474ed37f7a075031d2de7027a2
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-05-10 17:10:04 +1000

    Added screen for updating user profile
    added UI screen for admin to be able to update user profiles. frontend functionality still needs to be added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const App = () => {
   const userLogin = useSelector((state) => state.userLogin);
@@ -39,6 +40,7 @@ const App = () => {
               <Route path='/product/:id' element={<ProductScreen />} />
               <Route path='/cart/:id' element={<CartScreen />} />
               <Route path='/admin/userlist' element={<UserListScreen />} />
+              <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
               <Route path='/cart' element={<CartScreen />} />
               <Route path='/' element={<HomeScreen />} exact />
             </Routes>
