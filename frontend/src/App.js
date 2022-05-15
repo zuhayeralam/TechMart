6f62143925be677a8f0b20a762029ef13aef6f5f
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-05-15 21:30:00 +1000

    Added product update or edit screen
    when admin creates a new product or wants to update a product, it will take them to the ProductEditScreen where  they can edit that product by id.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
   const userLogin = useSelector((state) => state.userLogin);
@@ -45,6 +46,10 @@ const App = () => {
               <Route
                 path='/admin/productlist'
                 element={<ProductListScreen />}
+              />
+              <Route
+                path='/admin/product/:id/edit'
+                element={<ProductEditScreen />}
               />
               <Route path='/cart' element={<CartScreen />} />
               <Route path='/' element={<HomeScreen />} exact />
