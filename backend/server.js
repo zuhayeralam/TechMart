b8fa87ab8fbf08d2686eb12fda820be91c80a32f
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-05-31 23:35:22 +1000

    Added morgan to see server requests

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -15,6 +16,10 @@ connectDB();
 
 const app = express();
 app.use(express.json()); //allow us to accept json data in the body for auth
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...');
